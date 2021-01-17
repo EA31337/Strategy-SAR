beb33b47726aa3664b7d1a4efcf9f8de301e73af
kenorb <kenorb@users.noreply.github.com>
2021-01-17 19:51:26 +0000

    Optimize Indicator params (pf=0.37; dd=3.50; tt=2921)

--- config/EURUSD_M1.h
+++ config/EURUSD_M1.h
@@ -1,4 +1,4 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
@@ -6,8 +6,8 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_SAR_Params_M1 : SARParams {
   Indi_SAR_Params_M1() : SARParams(indi_sar_defaults, PERIOD_M1) {
-    step = (float)0.05;
-    max = (float)0.5;
+    step = 0.01;
+    max = 0.1;
     shift = 0;
   }
 } indi_sar_m1;
@@ -19,12 +19,12 @@ struct Stg_SAR_Params_M1 : StgParams {
     lot_size = 0;
     signal_open_method = 0;
     signal_open_filter = 1;
-    signal_open_level = (float)0;
+    signal_open_level = (float)0.0;
     signal_open_boost = 0;
     signal_close_method = 0;
     signal_close_level = (float)0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)1;
     tick_filter_method = 1;
     max_spread = 0;
   }
